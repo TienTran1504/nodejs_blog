16b0999818e7a169ca3b7971cf28dafa6ce5a6e9
TienTran1504 <trandungtien1504@gmail.com>
2022-09-26 13:57:14 +0700

    Morgan

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// app.use(morgan('combined'))
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
